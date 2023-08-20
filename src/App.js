d15b57e7be246a5f415130b3de1727f83e4ee011
anandmurti012 <anand.raj431@gmail.com>
2023-08-20 14:18:12 +0530

    json added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
+      <h1 style={{color:'lightgreen'}}>Welcome to Dairy App</h1>
+      <form>
+        <input type="text" placeholder="userName" name="username" />
+        <input type="email" placeholder="userEmail" name="useremail" />
+        <input type="password" placeholder="Enter your password" name="password" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
